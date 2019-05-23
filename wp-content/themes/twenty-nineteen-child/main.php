ba3d67bcd7d808acf46a02f18ad439ccd16ec780
Shawn Meyer <srbmwins@gmail.com>
2019-05-23 14:39:01 -0700

    "main section image, text, styles"

--- wp-content/themes/twenty-nineteen-child/main.php
+++ wp-content/themes/twenty-nineteen-child/main.php
@@ -4,6 +4,19 @@ Template Name: Main
 */
  get_header();
 ?>
+<section class="main">
+  <?php
+
+  if (have_posts()) :
+     while (have_posts()) :
+        the_post();
+           the_content();
+     endwhile;
+  endif;
+  ?>
+
+</section>
+</div>   <!-- From header.php -->
 
 <?php
  get_footer();
